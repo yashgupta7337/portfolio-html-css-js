ffc7ba9f0598590ab04f0e5968ae041091a642c0
Yash Gupta <ID+username@users.noreply.github.com>
2024-03-27 16:33:45 +0530

    update resume and education logos according to light and dark modes -> images and html, css, js logic

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let lightMode = false;
 const navbar = document.getElementsByClassName("main-nav");
+const logoContainers = document.getElementsByClassName("logo-container");
 Array.from(navbar).forEach((element) => {
   element.addEventListener("click", toggleTheme);
 });
@@ -14,6 +15,14 @@ function toggleTheme(event) {
     document.querySelector(".email-link").style.color = "#000000f2";
     document.querySelector("#resume").style.color = "#000000f2";
     //logo-container background color
+    Array.from(logoContainers).forEach((container) => {
+      container.style.backgroundColor = "#a6a6a6";
+    });
+    //resume, education icons color
+    document.querySelector("#resume-logo").src =
+      "assets/resume-light-mode-icon.png";
+    document.querySelector("#education-logo").src =
+      "assets/education-light-mode-logo.png";
   } else {
     lightMode = false;
     // background and text color
@@ -22,5 +31,14 @@ function toggleTheme(event) {
     //navbar and main-nav items color
     document.querySelector(".email-link").style.color = "#E0E0E0";
     document.querySelector("#resume").style.color = "#E0E0E0";
+    //logo-container background color
+    Array.from(logoContainers).forEach((container) => {
+      container.style.backgroundColor = "#E0E0E0";
+    });
+    //resume, education icons color
+    document.querySelector("#resume-logo").src =
+      "assets/resume-dark-mode-logo.png";
+    document.querySelector("#education-logo").src =
+      "assets/education-dark-mode-logo.png";
   }
 }
