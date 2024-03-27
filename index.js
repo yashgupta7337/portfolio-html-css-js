5c51d90229bc1e58fcbd369d27992345f4b99d65
Yash Gupta <ID+username@users.noreply.github.com>
2024-03-27 16:44:47 +0530

    memoji border color toggleTheme, css redundancy fix, update memoji img

--- index.js
+++ index.js
@@ -9,20 +9,22 @@ function toggleTheme(event) {
   if (lightMode === false) {
     lightMode = true;
     // background and text color
-    document.body.style.backgroundColor = "#E0E0E0";
+    document.body.style.backgroundColor = "#f2f2f2";
     document.body.style.color = "#000000f2";
     //navbar and main-nav items color
     document.querySelector(".email-link").style.color = "#000000f2";
     document.querySelector("#resume").style.color = "#000000f2";
     //logo-container background color
     Array.from(logoContainers).forEach((container) => {
-      container.style.backgroundColor = "#a6a6a6";
+      container.style.backgroundColor = "#f2f2f2";
     });
     //resume, education icons color
     document.querySelector("#resume-logo").src =
       "assets/resume-light-mode-icon.png";
     document.querySelector("#education-logo").src =
       "assets/education-light-mode-logo.png";
+    //memoji border color
+    document.querySelector(".logo img").style.border = "10px solid #66ffff";
   } else {
     lightMode = false;
     // background and text color
@@ -40,5 +42,7 @@ function toggleTheme(event) {
       "assets/resume-dark-mode-logo.png";
     document.querySelector("#education-logo").src =
       "assets/education-dark-mode-logo.png";
+    //memoji border color
+    document.querySelector(".logo img").style.border = "10px solid #E0E0E0";
   }
 }
